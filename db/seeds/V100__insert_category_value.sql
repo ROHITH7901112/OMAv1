e7b6b60a91078ef92fddbc2120771ac6ee042908
Madhanraj37 <rajmadhan296gmail.com>
2026-02-12 12:45:15 +0530

    category table created

---
+++ db/seeds/V100__insert_category_value.sql
@@ -0,0 +1,9 @@
+insert into category(category_text) values
+('Strategic Leadership and vision'),
+('Culture Integration'),
+('Governance and Decision Making'),
+('Leadership Capability and Succession'),
+('Change Agility'),
+('Communication and Engagement'),
+('Performace and Accountability'),
+('Growth and Progress');
