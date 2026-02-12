1ba666be1e737ee96ccbae3a530b6b1c8b5e3442
Madhanraj37 <rajmadhan296gmail.com>
2026-02-12 14:27:02 +0530

    main qns, sub qns, options tables created

---
+++ db/migrations/V2__mainquestion_tab_creation.sql
@@ -0,0 +1,12 @@
+CREATE TABLE mainquestion (
+    main_question_id SERIAL PRIMARY KEY,
+    category_id INT NOT NULL,
+    question_text TEXT NOT NULL,
+    question_type VARCHAR(50) NOT NULL, 
+    weight INT DEFAULT 0,
+ 
+    CONSTRAINT fk_main_question_category
+        FOREIGN KEY (category_id)
+        REFERENCES category(category_id)
+        ON DELETE CASCADE
+);
