9b04b6dd9ff50131cbb1abbd577bd6b3d1511a81
naresh0908 <nk09082005@gmail.com>
2026-02-12 16:01:43 +0530

    insert subquestions

---
+++ db/seeds/V103__insert_subquestion_value.sql
@@ -0,0 +1,227 @@
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I feel involved in shaping the company’s direction and priorities.'
+FROM mainquestion
+WHERE question_text = 'You as a leader, how would you rate the below statements in relation to EVORA translating its vision into actionable and measurable steps?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I am clear and aligned on where the company is heading.'
+FROM mainquestion
+WHERE question_text = 'You as a leader, how would you rate the below statements in relation to EVORA translating its vision into actionable and measurable steps?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I believe that I am empowered to influence strategy, and not just follow it.'
+FROM mainquestion
+WHERE question_text = 'You as a leader, how would you rate the below statements in relation to EVORA translating its vision into actionable and measurable steps?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'There is a culture among leaders to collaborate and adapt our plans when market changes happen.'
+FROM mainquestion
+WHERE question_text = 'You as a leader, how would you rate the below statements in relation to EVORA translating its vision into actionable and measurable steps?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I am encouraged to engage my teams to present new and innovative ideas.'
+FROM mainquestion
+WHERE question_text = 'You as a leader, how would you rate the below statements in relation to EVORA translating its vision into actionable and measurable steps?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'The leadership team has a shared understanding of our long-term goals.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Each business unit or function understands how it contributes to the overall strategy.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Strategic decisions are not limited to the top few; all leaders have a voice.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Strategic discussions include open debate and diversity of thought.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders are confident and well-equipped to explain strategy to their teams.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Strategic adjustments are made proactively, not reactively.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders work together to prioritize what truly matters when market conditions shift.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders maintain focus on the long-term vision even during short-term disruptions.'
+FROM mainquestion
+WHERE question_text = 'Having identified the leadership approach at EVORA, how would you rate the below statements?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'EVORA''s organisational culture ensures delegation of decision-making power appropriately.'
+FROM mainquestion
+WHERE question_text = 'What in your opinion and experience drives the EVORA leadership''s decision making policy?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders are empowered to make decisions within their areas.'
+FROM mainquestion
+WHERE question_text = 'What in your opinion and experience drives the EVORA leadership''s decision making policy?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders make decisions together, based on good discussion and data.'
+FROM mainquestion
+WHERE question_text = 'What in your opinion and experience drives the EVORA leadership''s decision making policy?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders are encouraged to communicate decisions clearly to the rest of the business.'
+FROM mainquestion
+WHERE question_text = 'What in your opinion and experience drives the EVORA leadership''s decision making policy?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Cross-functional collaborative decision making is followed across entities.'
+FROM mainquestion
+WHERE question_text = 'What in your opinion and experience drives the EVORA leadership''s decision making policy?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Teamwork and Trust'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Innovation and Problem-Solving'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Learning and Growth'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Engagement and Motivation'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Psychological Safety'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Adaptability and Resilience'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Sustainable Growth'
+FROM mainquestion
+WHERE question_text = 'Please support your choice in the previous question by rating EVORA on the following parameters.';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I am encouraged and supported to grow my skills.'
+FROM mainquestion
+WHERE question_text = 'Looking at EVORA''s long-term vision, how would you rate below statements about EVORA''s leadership skill, succession and readiness?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I feel motivated to actively help my teams prepare for bigger roles.'
+FROM mainquestion
+WHERE question_text = 'Looking at EVORA''s long-term vision, how would you rate below statements about EVORA''s leadership skill, succession and readiness?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leadership works together to decide on who could take over key roles when needed.'
+FROM mainquestion
+WHERE question_text = 'Looking at EVORA''s long-term vision, how would you rate below statements about EVORA''s leadership skill, succession and readiness?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I learn from other leaders and share leadership lessons.'
+FROM mainquestion
+WHERE question_text = 'Looking at EVORA''s long-term vision, how would you rate below statements about EVORA''s leadership skill, succession and readiness?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leadership capability building is treated as a strategic priority.'
+FROM mainquestion
+WHERE question_text = 'Looking at EVORA''s long-term vision, how would you rate below statements about EVORA''s leadership skill, succession and readiness?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I feel supported to lead change initiatives confidently.'
+FROM mainquestion
+WHERE question_text = 'With the constantly changing technology and digital market, how in your opinion does EVORA respond to change?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders talk openly about challenges and support each other through transitions.'
+FROM mainquestion
+WHERE question_text = 'With the constantly changing technology and digital market, how in your opinion does EVORA respond to change?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders come together to manage changes from scaling to market stimulus.'
+FROM mainquestion
+WHERE question_text = 'With the constantly changing technology and digital market, how in your opinion does EVORA respond to change?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I am supported to help my teams adjust well to change.'
+FROM mainquestion
+WHERE question_text = 'With the constantly changing technology and digital market, how in your opinion does EVORA respond to change?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I feel equipped to adapt quickly to new market realities, technologies, or integrations.'
+FROM mainquestion
+WHERE question_text = 'With the constantly changing technology and digital market, how in your opinion does EVORA respond to change?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'There is regular and consistent communication flow of information in EVORA.'
+FROM mainquestion
+WHERE question_text = 'How does the leadership at EVORA communicate internally and externally?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I believe my feedback is taken positively and can make a difference.'
+FROM mainquestion
+WHERE question_text = 'How does the leadership at EVORA communicate internally and externally?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'I feel interested and excited to learn and know more about EVORA''s latest updates.'
+FROM mainquestion
+WHERE question_text = 'How does the leadership at EVORA communicate internally and externally?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Communication across tiers is timely, transparent and structured.'
+FROM mainquestion
+WHERE question_text = 'How does the leadership at EVORA communicate internally and externally?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Communication is done in a way that builds trust and understanding.'
+FROM mainquestion
+WHERE question_text = 'How does the leadership at EVORA communicate internally and externally?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Goals, OKRs and KPIs are clearly defined and cascaded.'
+FROM mainquestion
+WHERE question_text = 'What kind of accountability systems are in place at EVORA?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders are consistently held accountable for results.'
+FROM mainquestion
+WHERE question_text = 'What kind of accountability systems are in place at EVORA?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Leaders use data to drive performance improvements.'
+FROM mainquestion
+WHERE question_text = 'What kind of accountability systems are in place at EVORA?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Accountability is balanced with empowerment.'
+FROM mainquestion
+WHERE question_text = 'What kind of accountability systems are in place at EVORA?';
+
+INSERT INTO sub_question (main_question_id, question_text)
+SELECT main_question_id, 'Underperformance is handled constructively and fairly.'
+FROM mainquestion
+WHERE question_text = 'What kind of accountability systems are in place at EVORA?';
+
+
+
